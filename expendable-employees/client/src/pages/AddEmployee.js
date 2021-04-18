b20c93b943669646a5742c56e51dd331a8127a9c
Kevin Chandra <44078398+Sclatch@users.noreply.github.com>
2021-04-18 14:28:17 -0400

    Small Change on AddEmployee
    Changed the design of Addemployee page so that it matches with the general design

--- expendable-employees/client/src/pages/AddEmployee.js
+++ expendable-employees/client/src/pages/AddEmployee.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Divider, Typography, TextField, Button } from "@material-ui/core";
+import { Paper, Divider, Typography, TextField, Button } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import {Link} from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
@@ -7,11 +7,14 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
     appBarSpacer: theme.mixins.toolbar,
     content: {
-        marginLeft: theme.spacing(10),
-        marginRight: theme.spacing(10),
+        marginLeft: theme.spacing(5),
+        marginRight: theme.spacing(5),
         overflowX: 'hidden',
         overflowY: 'hidden',
         marginBottom: theme.spacing(5)
+    },
+    paperStyle: {
+        padding: theme.spacing(5)
     },
     dividerStyle: {
         marginTop: theme.spacing(2),
@@ -36,105 +39,109 @@ export default function AddEmployee() {
 
                 <Grid container spacing={10} justify="space-evenly">
                     <Grid item xs>
-                        <Typography variant="h5" gutterBottom={true}>Employee Information</Typography>
-                        <Grid container spacing={2}justify="space-between">
-                            <Grid item xs>
-                                <TextField
+                        <Paper className={classes.paperStyle}>
+                            <Typography variant="h5" gutterBottom={true}>Employee Information</Typography>
+                            <Grid container spacing={2}justify="space-between">
+                                <Grid item xs>
+                                    <TextField
+                                        fullWidth={true}
+                                        variant="outlined"
+                                        margin="dense"
+                                        required
+                                        name="firstname"
+                                        label="First Name"
+                                        id="firstname"
+                                        color="secondary"/>
+                                </Grid>
+                                <Grid item xs>
+                                    <TextField
                                     fullWidth={true}
                                     variant="outlined"
                                     margin="dense"
                                     required
-                                    name="firstname"
-                                    label="First Name"
-                                    id="firstname"
+                                    name="lastname"
+                                    label="Last Name"
+                                    id="lastname"
                                     color="secondary"/>
+                                </Grid>
                             </Grid>
-                            <Grid item xs>
-                                <TextField
+                            <Grid container spacing={2} justify="space-between">
+                                <Grid item xs>
+                                    <TextField
+                                        fullWidth={true}
+                                        variant="outlined"
+                                        margin="dense"
+                                        required
+                                        name="dateofbirth"
+                                        label="Date of Birth"
+                                        id="dateofbirth"
+                                        type="date"
+                                        color="secondary"
+                                        InputLabelProps={{
+                                            shrink: true,
+                                        }}/>
+                                    <TextField
+                                        fullWidth={true}
+                                        variant="outlined"
+                                        margin="dense"
+                                        required
+                                        name="phone"
+                                        label="Phone Number"
+                                        id="phone"
+                                        type="tel"
+                                        color="secondary"/>
+                                </Grid>
+                                <Grid item xs/>
+                            </Grid>
+                            <Divider className={classes.dividerStyle}/>
+                            <TextField
                                 fullWidth={true}
                                 variant="outlined"
                                 margin="dense"
                                 required
-                                name="lastname"
-                                label="Last Name"
-                                id="lastname"
+                                name="address"
+                                label="Address"
+                                id="address"
                                 color="secondary"/>
+                            <Grid container spacing={2} justify="space-between">
+                                <Grid item xs>
+                                    <TextField
+                                        fullWidth={true}
+                                        variant="outlined"
+                                        margin="dense"
+                                        required
+                                        name="postalcode"
+                                        label="Postal Code"
+                                        id="postalcode"
+                                        color="secondary"/>
+                                </Grid>
+                                <Grid item xs/>
                             </Grid>
-                        </Grid>
-                        <Grid container spacing={2} justify="space-between">
-                            <Grid item xs>
-                                <TextField
-                                    fullWidth={true}
-                                    variant="outlined"
-                                    margin="dense"
-                                    required
-                                    name="dateofbirth"
-                                    label="Date of Birth"
-                                    id="dateofbirth"
-                                    type="date"
-                                    color="secondary"
-                                    InputLabelProps={{
-                                        shrink: true,
-                                    }}/>
-                                <TextField
-                                    fullWidth={true}
-                                    variant="outlined"
-                                    margin="dense"
-                                    required
-                                    name="phone"
-                                    label="Phone Number"
-                                    id="phone"
-                                    type="tel"
-                                    color="secondary"/>
-                            </Grid>
-                            <Grid item xs/>
-                        </Grid>
-                        <Divider className={classes.dividerStyle}/>
-                        <TextField
-                            fullWidth={true}
-                            variant="outlined"
-                            margin="dense"
-                            required
-                            name="address"
-                            label="Address"
-                            id="address"
-                            color="secondary"/>
-                        <Grid container spacing={2} justify="space-between">
-                            <Grid item xs>
-                                <TextField
-                                    fullWidth={true}
-                                    variant="outlined"
-                                    margin="dense"
-                                    required
-                                    name="postalcode"
-                                    label="Postal Code"
-                                    id="postalcode"
-                                    color="secondary"/>
-                            </Grid>
-                            <Grid item xs/>
-                        </Grid>
+                        </Paper>
                     </Grid>
                     <Grid item xs>
-                        <Typography variant="h5" gutterBottom={true}>Job Information</Typography>
-                        <TextField
-                            fullWidth={true}
-                            variant="outlined"
-                            margin="dense"
-                            required
-                            name="companyname"
-                            label="Company Name"
-                            id="companyname"
-                            color="secondary"/>
-                        <br/>
-                        <TextField
-                            fullWidth={true}
-                            variant="outlined"
-                            margin="dense"
-                            required
-                            name="position"
-                            label="Position"
-                            id="position"
-                            color="secondary"/>
+                        <Paper className={classes.paperStyle}>
+                            <Typography variant="h5" gutterBottom={true}>Job Information</Typography>
+                            <TextField
+                                fullWidth={true}
+                                variant="outlined"
+                                margin="dense"
+                                required
+                                name="companyname"
+                                label="Company Name"
+                                id="companyname"
+                                color="secondary"/>
+                            <br/>
+                            <TextField
+                                fullWidth={true}
+                                variant="outlined"
+                                margin="dense"
+                                required
+                                name="position"
+                                label="Position"
+                                id="position"
+                                color="secondary"/>
+                        </Paper>
                     </Grid>
                 </Grid>
                 <Grid container justify="flex-start">
