b99b39fc01cd70df8446e390bde57b67f056bd08
Kevin Chandra <44078398+Sclatch@users.noreply.github.com>
2021-04-19 01:08:46 -0400

    Partial Account
    + Topbar ID at least have some design
    + Tab buttons are now properly in the center

--- expendable-employees/client/src/components/TabsAccount.js
+++ expendable-employees/client/src/components/TabsAccount.js
@@ -8,14 +8,9 @@ Change Password
 */
 
 import React from "react";
-import {makeStyles, Button, Toolbar } from '@material-ui/core/';
-import {Container, Grid, Paper } from '@material-ui/core';
-import {Table, TableHead, TableBody, TableCell, TableRow, Typography} from '@material-ui/core';
-import {Link} from '@material-ui/core';
-import clsx from "clsx";
+import {makeStyles, Button, Grid, Paper } from '@material-ui/core/';
 import * as d3 from 'd3'; 
 
-import Title from "./Title";
 import QuitJob from "./QuitJob";
 import EditInfo from "./EditInfo";
 import PayInfo from "./PayInfo";
@@ -28,64 +23,12 @@ function preventDefault(event) {
 }
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-    },
-    toolbar: {
-        paddingRight: 24, // keep right padding when drawer closed
-    },
-    toolbarIcon: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-        padding: '0 8px',
-        ...theme.mixins.toolbar,
-    },
-    appBar: {
-        zIndex: theme.zIndex.drawer + 1,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    },
-    menuButton: {
-        marginRight: 36,
-    },
-    menuButtonHidden: {
-        display: 'none',
-    },
-    title: {
-        flexGrow: 1,
-    },
-    drawerPaperClose: {
-        overflowX: 'hidden',
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        width: theme.spacing(7),
-        [theme.breakpoints.up('sm')]: {
-            width: theme.spacing(9),
-        },
-    },
-    appBarSpacer: theme.mixins.toolbar,
-    content: {
-        flexGrow: 1,
-        height: '100vh',
-        overflow: 'auto',
-    },
-    container: {
-        paddingTop: theme.spacing(4),
-        paddingBottom: theme.spacing(4),
-    },
-    paper: {
-        padding: theme.spacing(2),
-        display: 'flex',
-        overflow: 'auto',
-        flexDirection: 'column',
-    },
-    fixedHeight: {
-        height: 240,
+    paperStyle: {
+        paddingTop: theme.spacing(2),
+        paddingBottom: theme.spacing(2),
+        marginLeft: theme.spacing(8),
+        marginRight: theme.spacing(8),
+        marginBottom: theme.spacing(2)
     }
 }));
 
@@ -318,12 +261,16 @@ export default function TabsAccount() {
 
         <React.Fragment>
             
-
-        <Toolbar>{buttonOptions.map((option) => (
-            <Button style={{ marginRight: "15%" }, {marginLeft: "10%"}} onClick= {loadComponent(option.component)}>
-                {option.buttonLabel}
-            </Button>
-        ))}</Toolbar>
+        <Paper className={classes.paperStyle}>
+            <Grid container justify="space-evenly">{buttonOptions.map((option) => (
+                <Grid item>
+                    <Button onClick= {loadComponent(option.component)}>
+                        {option.buttonLabel}
+                    </Button>
+                </Grid>
+            ))}
+            </Grid>
+        </Paper>
             
         <div id='quitJob' style={{display:'none'}}>
             <QuitJob></QuitJob>
--- expendable-employees/client/src/components/TopbarID.js
+++ expendable-employees/client/src/components/TopbarID.js
@@ -8,88 +8,26 @@ Phone Number
 */
 
 import React, { useEffect, useState } from "react";
-import {makeStyles, Button } from '@material-ui/core/';
-import {Container, Grid, Paper } from '@material-ui/core';
-import {Table, TableHead, TableBody, TableCell, TableRow, Typography} from '@material-ui/core';
-import {Card, CardActionArea, CardContent, CardMedia} from '@material-ui/core';
-import {Link} from '@material-ui/core';
-import clsx from "clsx";
-
-import Title from "../components/Title";
-
-import pfp from './pfp.png'
+import {makeStyles, Container, Grid, Paper, Typography, Divider, Avatar } from '@material-ui/core';
 
 import {setUserSession, getUserToken} from "../utils/userSession";
 import {useHistory} from 'react-router-dom';
 import axios from "axios";
 
-function preventDefault(event) {
-    event.preventDefault();
-}
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
+    avatar:{
+        width: '150px',
+        height: '150px'
     },
-    image:{
-
+    paperStyle: {
+        marginLeft: theme.spacing(8),
+        marginRight: theme.spacing(8),
+        padding: theme.spacing(4)
     },
-    toolbar: {
-        paddingRight: 24, // keep right padding when drawer closed
-    },
-    toolbarIcon: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-        padding: '0 8px',
-        ...theme.mixins.toolbar,
-    },
-    appBar: {
-        zIndex: theme.zIndex.drawer + 1,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    },
-    menuButton: {
-        marginRight: 36,
-    },
-    menuButtonHidden: {
-        display: 'none',
-    },
-    title: {
-        flexGrow: 1,
-    },
-    drawerPaperClose: {
-        overflowX: 'hidden',
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        width: theme.spacing(7),
-        [theme.breakpoints.up('sm')]: {
-            width: theme.spacing(9),
-        },
-    },
-    appBarSpacer: theme.mixins.toolbar,
-    content: {
-        flexGrow: 1,
-        height: '100vh',
-        overflow: 'auto',
-    },
-    container: {
-        paddingTop: theme.spacing(4),
-        paddingBottom: theme.spacing(4),
-    },
-    paper: {
-        padding: theme.spacing(2),
-        display: 'flex',
-        overflow: 'auto',
-        flexDirection: 'column',
-    },
-    fixedHeight: {
-        height: 240,
-    },
+    dividerStyle: {
+        margin: theme.spacing(1)
+    }
 }));
 
 /*
@@ -164,22 +102,23 @@ export default function TopbarID() {
     console.log(data)
     return(
         <React.Fragment>
-            <Container>
-                <Grid container spacing={3}>
-               
-                    <Grid item xs={6}>
+            <Paper className={classes.paperStyle}>
+                <Grid container alignItems="center" justify="flex-start">
+                    <Grid item xs>
+                        {/* this is where the pfp will be, i don't know how to do images right now  */}
+                        {/*<Typography>Placeholder for pfp</Typography>*/}
+                        <Avatar className={classes.avatar}>Placeholder</Avatar>
+                    </Grid>
+                    <Grid item xs>
                         {/* user info will be here, maybe a table */}
-                        <Typography>{data.firstname} {data.lastname} </Typography>
-                        <Typography>{data.position}</Typography><br/><br/>
+                        <Typography variant="h3">{data.firstname} {data.lastname} </Typography>
+                        <Divider className={classes.dividerStyle}/>
+                        <Typography>{data.position}</Typography>
                         <Typography>{data.email}</Typography>
                         <Typography>{data.phone}</Typography>
-                        </Grid>
-                    <Grid item xs={3}>
-                        {/* intentionally empty */}
                     </Grid>
                 </Grid>
-            </Container>
-            
+            </Paper>
         </React.Fragment>
     )
 
